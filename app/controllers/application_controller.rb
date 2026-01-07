e0fec819b50145c9981f2826a7a7b9255f975a6e
Esteban Soto <esoto074@gmail.com>
2026-01-07 10:52:30 -0600

    Add navigation header with auth links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  helper_method :current_user, :authenticated?
+
   private
 
   def current_user
     Current.user
   end
-  helper_method :current_user
+
+  def authenticated?
+    Current.user.present?
+  end
 end
