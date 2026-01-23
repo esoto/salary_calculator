5b5e327aed494d8c2840223e5ed14032906d8d2f
Esteban Soto <esoto074@gmail.com>
2026-01-23 14:43:39 -0600

    feat(view): add vacation over-limit warning banner with acknowledgment checkbox

--- spec/views/salary_entries/_form.html.erb_spec.rb
+++ spec/views/salary_entries/_form.html.erb_spec.rb
@@ -50,4 +50,46 @@ RSpec.describe "salary_entries/_form", type: :view do
       end
     end
   end
+
+  describe "vacation over-limit warning banner" do
+    let(:user) { create(:user, vacation_enabled: true) }
+    let(:salary_entry) { user.salary_entries.build(year: 2025, month: 1) }
+
+    before do
+      assign(:salary_entry, salary_entry)
+    end
+
+    context "when not over vacation limit" do
+      before do
+        assign(:over_vacation_limit, false)
+      end
+
+      it "does not show warning banner" do
+        render partial: "salary_entries/form", locals: { salary_entry: salary_entry }
+        expect(rendered).not_to have_css(".bg-amber-50")
+        expect(rendered).not_to have_content("Vacation days warning")
+      end
+    end
+
+    context "when over vacation limit" do
+      before do
+        assign(:over_vacation_limit, true)
+        assign(:vacation_over_by, 2.5)
+      end
+
+      it "shows warning banner with correct message" do
+        render partial: "salary_entries/form", locals: { salary_entry: salary_entry }
+        expect(rendered).to have_css(".bg-amber-50")
+        expect(rendered).to have_content("Vacation days warning")
+        expect(rendered).to have_content("You're taking 2.5")
+        expect(rendered).to have_content("more days than earned")
+      end
+
+      it "shows acknowledgment checkbox" do
+        render partial: "salary_entries/form", locals: { salary_entry: salary_entry }
+        expect(rendered).to have_field("salary_entry_vacation_over_limit_acknowledged")
+        expect(rendered).to have_content("I understand this affects my savings")
+      end
+    end
+  end
 end
