f2851b9b2d351fc2f53a67d8901e71f1a97e0969
Esteban Soto <esoto074@gmail.com>
2026-01-24 07:11:23 -0600

    update rails misc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_21_181528) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_24_130804) do
   create_table "salary_entries", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.decimal "holiday_days_taken", precision: 4, scale: 2, default: "0.0", null: false
