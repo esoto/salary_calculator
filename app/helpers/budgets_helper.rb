0f91c73e055b86084d2a238f9868062c649ab69f
Esteban Soto <esoto074@gmail.com>
2026-02-02 21:22:00 -0600

    feat: add activity display helpers for PaperTrail versions
    Add helper methods to format version records for activity log:
    - activity_description: formats changes into readable text
    - activity_actor_name: shows who made the change
    - activity_time_ago: relative time formatting
    
    Also adds object_changes column to versions table for change tracking.

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -62,4 +62,98 @@ module BudgetsHelper
   def budget_shared_badge
     content_tag(:span, "🏠 Shared", class: "text-xs px-2 py-1 bg-blue-100 text-blue-700 rounded")
   end
+
+  def activity_description(version)
+    case version.item_type
+    when "MonthlyBudget"
+      budget_activity_description(version)
+    when "BudgetItem"
+      budget_item_activity_description(version)
+    else
+      "Unknown activity"
+    end
+  end
+
+  def activity_actor_name(version, current_user)
+    return "You" if version.whodunnit == current_user.id.to_s
+
+    user = User.find_by(id: version.whodunnit)
+    user&.name || "Someone"
+  end
+
+  def activity_time_ago(version)
+    time_ago_in_words(version.created_at) + " ago"
+  end
+
+  private
+
+  def budget_activity_description(version)
+    case version.event
+    when "create"
+      "created this budget"
+    when "update"
+      changes = parse_version_changes(version)
+      if changes.key?("exchange_rate")
+        old_rate, new_rate = changes["exchange_rate"]
+        "changed exchange rate from #{old_rate.to_i} to #{new_rate.to_i}"
+      elsif changes.key?("shared_with_household")
+        changes["shared_with_household"][1] ? "shared this budget with household" : "made this budget private"
+      else
+        "updated this budget"
+      end
+    when "destroy"
+      "deleted this budget"
+    else
+      "modified this budget"
+    end
+  end
+
+  def budget_item_activity_description(version)
+    item_name = extract_item_name(version)
+
+    case version.event
+    when "create"
+      amount = extract_item_amount(version)
+      "added \"#{item_name}\" ($#{amount})"
+    when "update"
+      changes = parse_version_changes(version)
+      if changes.key?("amount")
+        old_amount, new_amount = changes["amount"]
+        "changed \"#{item_name}\" $#{old_amount} → $#{new_amount}"
+      elsif changes.key?("paid")
+        changes["paid"][1] ? "marked \"#{item_name}\" as paid" : "marked \"#{item_name}\" as unpaid"
+      else
+        "updated \"#{item_name}\""
+      end
+    when "destroy"
+      "removed \"#{item_name}\""
+    else
+      "modified \"#{item_name}\""
+    end
+  end
+
+  def extract_item_amount(version)
+    changes = parse_version_changes(version)
+    changes.dig("amount", 1) || version.item&.amount
+  end
+
+  def parse_version_changes(version)
+    return {} unless version.object_changes.present?
+
+    YAML.safe_load(
+      version.object_changes,
+      permitted_classes: [ BigDecimal, ActiveSupport::TimeWithZone, ActiveSupport::TimeZone, Time ],
+      aliases: true
+    ).except("updated_at", "created_at")
+  rescue StandardError
+    {}
+  end
+
+  def extract_item_name(version)
+    if version.event == "destroy"
+      version.reify&.name || "item"
+    else
+      version.item&.name || version.reify&.name || "item"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_02_222513) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_03_030626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -117,6 +117,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_02_222513) do
     t.bigint "item_id", null: false
     t.string "item_type", null: false
     t.text "object"
+    t.text "object_changes"
     t.string "whodunnit"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
